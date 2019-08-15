b8e217904972ccc164dea4604e66ca9a44010c55
Yulli Chong <yc@Yullis-MacBook-Pro.local>
2019-08-15 17:02:39 -0500

    created /recipes routes

---
+++ controllers/recipe.js
@@ -0,0 +1,38 @@
+//================================
+//         DEPENDENCIES
+//================================
+const express = require('express');
+const router = express.Router();
+const session = require('express-session');
+//================================
+//         DATA
+//================================
+
+//================================
+//         ROUTES
+//================================
+/////////  INDEX ROUTE ///////////
+router.get('/', (req, res) => {
+  Recipes.find({}, (error, foundRecipes) => {
+    res.json(foundRecipes)
+  })
+})
+//////// DELETE ROUTE ////////////
+router.delete('/:id', (req, res) => {
+  Recipes.findByIdAndRemove(req.params.id, (error, deletedRecipe) => {
+    res.json(deletedRecipe)
+  })
+})
+/////////// UPDATE ROUTE ////////////
+router.put('/:id', (req, res) => {
+  Recipes.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedRecipe) => {
+    res.json(updatedRecipe)
+  })
+})
+////////// CREATE ROUTE ////////////
+router.post('/', (req, res) => {
+  Recipes.create(req.body, (error, createdRecipe) => {
+    res.json(createdRecipe)
+  })
+})
+module.exports = router;
