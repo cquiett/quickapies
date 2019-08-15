f219c591637e945995d1ded9338071270157f3c0
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-08-15 16:10:13 -0400

    created basic layout and files for app

---
+++ server.js
@@ -0,0 +1,50 @@
+// =========================
+// Dependencies
+// =========================
+
+const bcypt = require("bcyrpt");
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+require("dotenv").config();
+
+const app = express();
+
+// =========================
+// Configurations
+// =========================
+
+const port = 3005;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once("open", () => {
+  console.log("Connected to Mongoose.");
+});
+
+// ==========================
+// Fix Deprecation Warnings
+// ==========================
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+// ==========================
+// Middleware
+// ==========================
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(session({
+    secret: ,
+    resave: false,
+    saveUninitialized: false
+  }));
+
+  // ==========================
+// Listener
+// ==========================
+
+app.listen(port, () => {
+  console.log("I'm totes listenin' on port: " + port);
+});
