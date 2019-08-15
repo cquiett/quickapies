96d44a2b9e2eaf31f05fe5cdad5fe14f01fb07fb
Becka Catanzaro <becka@Beckas-MacBook-Pro.local>
2019-08-15 16:48:29 -0400

    attempting to fix configuration issues

---
+++ server.js
@@ -0,0 +1,51 @@
+// =========================
+// Dependencies
+// =========================
+
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+require("dotenv").config();
+
+const app = express();
+
+// =========================
+// Configurations
+// =========================
+
+const port = process.env.PORT || 3005;
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.once("open", () => {
+  console.log("Connected to Mongoose.");
+});
+
+// ==========================
+// Fix Deprecation Warnings
+// ==========================
+
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+
+// ==========================
+// Middleware
+// ==========================
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+  }));
+
+// ==========================
+// Listener
+// ==========================
+
+app.listen(port, () => {
+  console.log("I'm totes listenin' on port: " + port);
+});
